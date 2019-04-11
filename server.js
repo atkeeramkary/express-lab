54bb35913b83c560ddacc8259cff320dc8057ef9
Anil Keeramkarye <atkeeramkary@gmail.comm>
2019-04-11 13:34:48 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+//Setting up the server to listen to requests from the client
+const express = require('express')
+
+const app = express()
+
+
+app.get('/greeting', (req, res) => {
+  res.send("Hello Stranger")
+});
+
+
+
+app.get('/greeting/:name', (req, res) =>{
+	const name ='Porter'
+	res.send("Hello Stranger " + req.params.name + "!")
+});
+
+
+
+app.listen(3000, () => {
+
+	console.log('Server is listening on Port 3000');
+});
