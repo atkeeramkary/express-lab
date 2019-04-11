d6a5dee28d1ae86fbfd81e424fad6a4c3da0c740
Anil Keeramkarye <atkeeramkary@gmail.comm>
2019-04-11 13:54:54 -0500

    Commit 3: Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -21,7 +21,17 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	res.send(req.params.tipPercentage)
 })
 
+app.get('/magic/:question', (req, res) => {
+	const responses = ["It is certain", "It is decidedly so", "Without a doubt", 
+	"Yes definitely","You may rely on it", "As I see it yes", "Most likely", 
+	"Outlook good","Yes", "Signs point to yes", "Reply hazy try again", 
+	"Ask again later","Better not tell you now", "Cannot predict now", 
+	"Concentrate and ask again","Don't count on it", "My reply is no", 
+	"My sources say no","Outlook not so good", "Very doubtful"]
 
+	const i = Math.floor(Math.random() * responses.length - 1)
+	res.send(req.params.question + "   " + responses[i])
+})
 
 app.listen(3000, () => {
 
